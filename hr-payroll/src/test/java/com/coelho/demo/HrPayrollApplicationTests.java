48ab8c3447f7409083ae8594df244a10480820ed
Wilson Coelho <wlfcoelho@gmail.com>
2022-08-15 10:47:46 -0300

    projeto hr-payroll criado

---
+++ hr-payroll/src/test/java/com/coelho/demo/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coelho.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
