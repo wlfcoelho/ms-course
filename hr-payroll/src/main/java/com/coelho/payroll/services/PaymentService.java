22342000253fba8ea4cbec13a832be6adb5f2b73
Wilson Coelho <wlfcoelho@gmail.com>
2022-08-15 15:20:02 -0300

    criação do feign client

--- hr-payroll/src/main/java/com/coelho/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/coelho/payroll/services/PaymentService.java
@@ -1,30 +1,22 @@
 package com.coelho.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.coelho.payroll.entities.Payment;
 import com.coelho.payroll.entities.Worker;
+import com.coelho.payroll.feignclients.WorkerFeingCleint;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplete;
+	private WorkerFeingCleint workerFeingCleint; 
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
 		
-		Worker worker = restTemplete.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		
+		Worker worker = workerFeingCleint.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
