0e7dd5092e1d64fb848b8e62a4a9b2bf1803ba69
Wilson Coelho <wlfcoelho@gmail.com>
2022-08-15 16:06:06 -0300

    balanceamento de carga com ribbon

--- hr-payroll/src/main/java/com/coelho/payroll/feignclients/WorkerFeingCleint.java
+++ hr-payroll/src/main/java/com/coelho/payroll/feignclients/WorkerFeingCleint.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.coelho.payroll.entities.Worker;
 
 @Component
-@FeignClient (name = "hr-worker", url= "localhost:8001", path= "/workers" )
+@FeignClient (name = "hr-worker", path= "/workers" )
 public interface WorkerFeingCleint {
 
 	@GetMapping(value = "/{id}")
-	ResponseEntity<Worker> findById(@PathVariable Long id);	
+	ResponseEntity<Worker> findById(@PathVariable Long id);
 }
--- hr-worker/src/main/java/com/coelho/demo/worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/coelho/demo/worker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.coelho.demo.worker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +19,13 @@ import com.coelho.demo.worker.repositories.WorkerRepository;
 @RequestMapping (value = "/workers")
 public class WorkerResource {
 	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+			
 	@Autowired
 	private WorkerRepository repository;
+	
+	@Autowired
+	private Environment env;
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
@@ -28,6 +36,8 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+		logger.info("PORT= "+ env.getProperty("local.server.port"));
 		Worker obj = repository.findById(id).get();
 		
 		return ResponseEntity.ok(obj);
