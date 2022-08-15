7a806ac9377cca8f41f01ceb12a1abc693c793fa
Wilson Coelho <wlfcoelho@gmail.com>
2022-08-15 16:12:20 -0300

    criando eureka

---
+++ hr-eureka-server/src/test/java/com/coelho/demo/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coelho.demo.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
