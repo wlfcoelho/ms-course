469ea0d98b1ed2f6f3bad816e0d67b45301464f3
Wilson Coelho <wlfcoelho@gmail.com>
2022-08-22 20:58:55 -0300

    Configuração eureka server

--- hr-eureka-server/src/main/java/com/example/hreureka/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/example/hreureka/HrEurekaServerApplication.java
@@ -1,8 +1,10 @@
-package com.coelho.demo.hreurekaserver;
+package com.example.hreureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
--- hr-eureka-server/src/test/java/com/example/hreureka/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/example/hreureka/HrEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.coelho.demo.hreurekaserver;
+package com.example.hreureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
